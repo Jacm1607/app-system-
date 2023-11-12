1619827be27f996cedb5e3a2e7b7c6772ab6acad
Jose <chavarriaj84@gmail.com>
2023-11-12 19:39:19 -0400

    [FEATURE] menu add option

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -409,6 +409,20 @@
                   <!-- active & hover classes 'text-gray-700 dark:text-light' -->
                   <!-- inActive classes 'text-gray-400 dark:text-gray-400' -->
                   <a
+                    href="{{route('usuario.index')}}"
+                    role="menuitem"
+                    class="block p-2 text-sm text-gray-400 transition-colors duration-200 rounded-md dark:hover:text-light hover:text-gray-700"
+                  >
+                    Gestión de Usuario
+                  </a>
+                  <a
+                    href="{{route('rol.index')}}"
+                    role="menuitem"
+                    class="block p-2 text-sm text-gray-400 transition-colors duration-200 rounded-md dark:hover:text-light hover:text-gray-700"
+                  >
+                    Gestión de Rol
+                  </a>
+                  <a
                     href="{{route('privilegio.index')}}"
                     role="menuitem"
                     class="block p-2 text-sm text-gray-400 transition-colors duration-200 rounded-md dark:hover:text-light hover:text-gray-700"
